b5215874192c40fe7630abf7648eddb9516d4ebd
Qi Gao <qi.gao2012@gmail.com>
2026-09-17 17:14:15 +0800

    fix: salts utils path missing

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -1,4 +1,3 @@
 option(BUILD_TESTS "Build test suite" ON)
 option(BUILD_EXAMPLES "Build example programs" ON)
 option(BUILD_BENCHMARKS "Build benchmark executables" OFF)
-option(BUILD_OPENAPI "Build the source annotation OpenAPI generator" OFF)
