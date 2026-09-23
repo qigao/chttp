025eca85f74ff3be76ba7e1786b74cc20ce691d0
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 22:06:04 +0800

    test(service): compile installed Service header

---
+++ service/tests/installed/main.c
@@ -0,0 +1,11 @@
+#include <chttp_service/service.h>
+
+int main(void) {
+  chttp_service service = {0};
+  chttp_service_config config = CHTTP_SERVICE_CONFIG_INIT;
+  chttp_service_http_method method = CHTTP_SERVICE_HTTP_METHOD_INIT;
+  (void)service;
+  (void)config;
+  (void)method;
+  return 0;
+}
