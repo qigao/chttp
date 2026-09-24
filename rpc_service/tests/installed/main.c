3a6cdb9a440fdec3dad9baa44f49f07183aa5989
Qi Gao <qi.gao2012@gmail.com>
2026-09-25 07:33:48 +0800

    test(rpc-service): compile installed mount API

---
+++ rpc_service/tests/installed/main.c
@@ -0,0 +1,11 @@
+#include <chttp_rpc_service/service.h>
+
+int main(void) {
+  chttp_rpc_service service = {0};
+  chttp_rpc_service_config config = CHTTP_RPC_SERVICE_CONFIG_INIT;
+  chttp_rpc_service_mount mount = CHTTP_RPC_SERVICE_MOUNT_INIT;
+  (void)service;
+  (void)config;
+  (void)mount;
+  return 0;
+}
